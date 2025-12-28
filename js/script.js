6dc82338f6e764ee0ba89a0a37cbb654c35798e2
rq1611 <rifqirajif2006@gmail.com>
2025-12-28 21:25:19 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// AOS Init
+AOS.init({
+  once: true,
+  duration: 900,
+  easing: "ease-out-cubic",
+});
+
+// Typing Effect
+const text = "Web Developer • student from Universitas Pasundan";
+const target = document.querySelector(".typing-text");
+let i = 0;
+
+function typing() {
+  if (i < text.length) {
+    target.textContent += text.charAt(i);
+    i++;
+    setTimeout(typing, 100);
+  }
+}
+typing();
+
+// Hamburger Menu
+const toggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+toggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
